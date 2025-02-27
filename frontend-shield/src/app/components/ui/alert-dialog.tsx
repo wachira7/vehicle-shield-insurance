86b4d3d0e6acb33c26232f6f5dcf5a75364b1b96
emmanuel-wachira <waruterewachira7@gmail.com>
2025-02-27 07:56:03 +0300

    ui-alertdialog edit

--- frontend-shield/src/app/components/ui/alert-dialog.tsx
+++ frontend-shield/src/app/components/ui/alert-dialog.tsx
@@ -14,9 +14,9 @@ const AlertDialogPortal = ({
   className,
   children,
   ...props
-}: AlertDialogPrimitive.AlertDialogPortalProps) => (
-  <AlertDialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="fixed inset-0 z-50 flex items-end justify-center sm:items-center">
+}: AlertDialogPrimitive.AlertDialogPortalProps & { className?: string }) => (
+  <AlertDialogPrimitive.Portal  {...props}>
+    <div className={cn("fixed inset-0 z-50 flex items-end justify-center sm:items-center",className)}>
       {children}
     </div>
   </AlertDialogPrimitive.Portal>
@@ -26,7 +26,7 @@ AlertDialogPortal.displayName = AlertDialogPrimitive.Portal.displayName
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
->(({ className, children, ...props }, ref) => (
+>(({ className,  ...props }, ref) => (
   <AlertDialogPrimitive.Overlay
     className={cn(
       "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm transition-opacity animate-in fade-in",
